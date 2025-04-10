8aae78712348b95dea06253953199901fbfc1ef7
zenith02-void <jsung2220@gmail.com>
2025-04-11 03:38:37 +0600

    Create wco-extension.js

---
+++ wco-extension.js
@@ -0,0 +1,13 @@
+window.latestExtension = {
+  name: "WCO TV",
+  getAnimeList: async () => [
+    {
+      title: "Naruto",
+      streamUrl: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+    },
+    {
+      title: "Bleach",
+      streamUrl: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+    }
+  ]
+};
